cce6043b6c8d103883b95f416c09ccace0243baf
Eduard <onedi_edono@yahoo.com>
2023-02-28 20:29:30 +0100

    Initial commit, generation with Spring initialzr (https://start.spring.io/)

---
+++ readme.md
@@ -0,0 +1,2 @@
+1. First, the project was generated with Spring initialzr (https://start.spring.io/).
+2. 
---
+++ src/test/java/com/inditex/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
